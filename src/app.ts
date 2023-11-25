2b0a4ea199a7a91c69e23e751c932a64116784ac
Sujoy-Kumar-Das <sujoykumardas75@gmial.com>
2023-11-25 22:37:15 +0600

    pakage.json file problem sloved

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import { userRouter } from './app/user/user.router';
-const app:Application = express();
+import { testRouter } from './testRouter';
+const app: Application = express();
 
 // express middlewares
 app.use(express.json());
 app.use(cors());
 
+app.use('/', testRouter);
 app.use('/api/users', userRouter);
 
 export default app;
